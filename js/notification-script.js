7ff02d202ef2b6836b3e5f2f0e526173be6720aa
YousefElbrolosy <yousefelbrolosy@gmail.com>
2024-05-06 16:31:15 +0300

    fixed some bugs

--- js/notification-script.js
+++ js/notification-script.js
@@ -8,11 +8,13 @@ $(document).ready(function () {
     $('#bell').click(function (e) {
 
         var color = $(this).text();
+
         if (down) {
 
             $('#box').css('height', '0px');
             $('#box').css('opacity', '0');
             $('#item').css('cursor', 'default');
+            $('.notifications').css('display','none')
             down = false;
 
         } else {
@@ -20,6 +22,7 @@ $(document).ready(function () {
             $('#box').css('height', 'auto');
             $('#box').css('opacity', '1');
             $('#item').css('cursor', 'pointer');
+            $('.notifications').css('display','block')
             down = true;
 
         }
